f6fb285033f29a513d0864b3dc3be76986fa15b6
yoonjookim <yjblossom@gmail.com>
2024-01-17 01:06:22 -0500

    test case changed

--- src/environment.js
+++ src/environment.js
@@ -1,5 +1,5 @@
 const path = require("path");
-
+console.log("environment.js >>> process.env.NODE_ENV :", process.env.NODE_ENV) // TODO remove later
 const ENV = process.env.NODE_ENV || "development";
 const PATH = path.resolve(__dirname, "../.env." + ENV);
 
--- src/routes/__tests__/days.test.js
+++ src/routes/__tests__/days.test.js
@@ -22,17 +22,38 @@ describe("Days", () => {
       .expect(200, [
         {
           id: 1,
-          name: "Monday",
-          appointments: [1, 2],
-          interviewers: [1, 2],
-          spots: 1
+          name: 'Monday',
+          appointments: [ 1, 2, 3, 4, 5 ],
+          interviewers: [ 1, 2, 5, 6, 7 ],
+          spots: 4
         },
         {
           id: 2,
-          name: "Tuesday",
-          appointments: [3, 4],
-          interviewers: [1, 2],
+          name: 'Tuesday',
+          appointments: [ 6, 7, 8, 9, 10 ],
+          interviewers: [ 3, 4, 7, 8, 10 ],
+          spots: 3
+        },
+        {
+          id: 3,
+          name: 'Wednesday',
+          appointments: [ 11, 12, 13, 14, 15 ],
+          interviewers: [ 2, 3, 6, 8, 9 ],
           spots: 2
+        },
+        {
+          id: 4,
+          name: 'Thursday',
+          appointments: [ 16, 17, 18, 19, 20 ],
+          interviewers: [ 1, 5, 6, 7, 8 ],
+          spots: 2
+        },
+        {
+          id: 5,
+          name: 'Friday',
+          appointments: [ 21, 22, 23, 24, 25 ],
+          interviewers: [ 2, 4, 5, 8, 10 ],
+          spots: 4
         }
       ]);
   });
